f69705951ead1725ba97022495786bac95d7d717
rishabh-malhotra <rishabh11.rm@gmail.com>
2024-03-20 01:40:11 +0530

    inital commit

---
+++ NZWalks/NZWalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,18 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double LengthInKm { get; set; }
+        public string? WalkImageUrl { get; set; }
+        public Guid DifficultyId { get; set; }
+        public Guid RegionId { get; set; }
+
+        //Navigation properties
+        public Difficulty Difficulty { get; set; } // tells ef core that a Walk will have difficulty
+        public Region Region { get; set; }
+
+    }
+}
